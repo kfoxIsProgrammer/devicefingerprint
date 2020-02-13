690d7c4dc8a9fc88a43e7c44e5640b5e148cfb4c
Kevin Fox <39808977+kfoxIsProgrammer@users.noreply.github.com>
2020-02-13 14:14:52 -0500

    Added some extra to do's

--- server/server.js
+++ server/server.js
@@ -1,28 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const { body,validationResult } = require('express-validator');
-const { sanitizeBody } = require('express-validator');
-var bodyParser = require('body-parser');
+const express = require('express') //Main api used
+const app = express()// app object
+const port = 3000// Port number used
+
+const { body,validationResult } = require('express-validator'); //WIll be used to validate queries
+const { sanitizeBody } = require('express-validator');// Will be used to fix queries
+
+var bodyParser = require('body-parser');// Used to get objects json objects from clients
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 // in latest body-parser use like below.
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var path = require('path');
+var path = require('path');// Get relative path to this file
 
 app.get('/', (req, res) =>  res.sendFile(path.resolve('../home.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
-
+//Script ran when form is sent from home.html
   app.post('/submit-form',function(request,response){
    console.log(request.body) 
    response.send("hi")
 
 
-
+// Mysql database connection, will refactor this to hide credentials later
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -30,10 +32,14 @@ const connection = mysql.createConnection({
   password: 'Foxer123',
   database: 'myusers'
 });
+
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected!');
 });
+
+//Need to get info from database about users credentials and compare for logging in
+//Then need to compare device fingerprint for validation
 
 })
   
