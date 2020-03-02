89e7cd69efb90fa7fc07910de9c6cb064eb890e1
Kevin Fox <39808977+kfoxIsProgrammer@users.noreply.github.com>
2020-03-01 23:15:14 -0500

    Mysql set up. Client to Server next

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
+'user strict';
 const express = require('express') //Main api used
 const app = express()// app object
 const port = 3000// Port number used
+const mysql = require('mysql');
+
 
 const { body,validationResult } = require('express-validator'); //WIll be used to validate queries
 const { sanitizeBody } = require('express-validator');// Will be used to fix queries
@@ -18,18 +21,33 @@ app.get('/', (req, res) =>  res.sendFile(path.resolve('../home.html')))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
+
+
+
+
+
 //Script ran when form is sent from home.html
   app.post('/submit-form',function(request,response){
-   console.log(request.body) 
+
+[
+  	 body('exampleInputEmail1')
+    .isEmail()
+    .normalizeEmail(),
+  body('text')
+    .not().isEmpty()
+    .trim()
+    .escape(),
+  body('notifyOnReply').toBoolean()
+] ;
+  console.log(request.body.exampleInputEmail1) 
    response.send("hi")
 
 
 // Mysql database connection, will refactor this to hide credentials later
-const mysql = require('mysql');
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Foxer123',
   database: 'myusers'
 });
 
@@ -38,6 +56,23 @@ connection.connect((err) => {
   console.log('Connected!');
 });
 
+connection.query('SELECT * from users where email = ?',[request.body.exampleInputEmail1], function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0])
+	console.log(request.body)
+})
+
+
+
+
+
+
+
+
+//var clean_user = sanitizeBody.sanitize(request.body.exampleInputEmail1);
+
+//console.log(sanitizeBody.sanitize(request.body.exampleInputEmail1));
 //Need to get info from database about users credentials and compare for logging in
 //Then need to compare device fingerprint for validation
 
